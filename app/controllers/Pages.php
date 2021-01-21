f0a10199a598f558c26e3b18a8c6ffe594946f74
nathanjh-28 <nathan@nathanjharris.com>
2021-01-21 14:19:19 -0700

    incorporated views in the page controller

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,11 +6,17 @@ class Pages extends Controller
     {
     }
     public function index()
+
+
     {
-        $this->view('hello');
+        $data = [
+            'title' => 'Welcome'
+        ];
+
+        $this->view('pages/index', $data);
     }
-    public function about($id)
+    public function about()
     {
-        echo $id;
+        $this->view('pages/about');
     }
 }
