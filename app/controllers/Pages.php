c90328df066a780bc9ae6c9f945353ccc5515e4b
nathanjh-28 <nathan@nathanjharris.com>
2021-01-29 15:57:07 -0700

    add post form complete

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,10 +6,10 @@ class Pages extends Controller
     {
         
     }
-    public function index()
-
-
-    {
+    public function index(){
+        if(isLoggedIn()){
+            redirect('posts');
+    }
         $data = [
             'title' => 'Share Posts',
             'description' => 'Simple social network built on the ohmy MVC framework.'
--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -17,6 +17,15 @@ class Posts extends Controller
         ];
         $this->view('posts/index', $data);
     }
+
+    public function add(){
+        $data = [
+            'title'=> '',
+            'body' => ''
+        ];
+        $this->view('posts/add', $data);
+
+    }
     
 
 }
