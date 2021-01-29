9730f2f6139f0700de8fc8beff805b0080eee683
nathanjh-28 <nathan@nathanjharris.com>
2021-01-29 14:38:30 -0700

    Post model and display index posts

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -12,5 +12,25 @@
     </div>
 </div>
 
+<?php foreach($data['posts'] as $post) : ?>
+
+<div class="card card-body mb-3">
+    <h4 class="card-title">
+    <?= $post->title; ?>
+    </h4>
+
+    <div class="bg-light p-2 mb-3">
+    
+    written by <?= $post->name; ?> on <?= $post->postCreated; ?>
+
+    </div>
+
+    <p class="card-text"><?=$post->body;?></p>
+    <a href="<?= URLROOT;?>/posts/show/<?=$post->postId;?>" class="btn btn-dark">More</a>
+
+</div>
+
+<?php endforeach; ?>
+
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
