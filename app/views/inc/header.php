cd4b69bb13fd82221a8e25779538f16d43c0e438
nathanjh-28 <nathan@nathanjharris.com>
2021-02-04 11:22:24 -0700

    css reset

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 
     <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css"> 
 
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <!-- <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script> -->
     <script defer src="<?= URLROOT;?>/js/main.js" ></script>
     
     
--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,35 +1,31 @@
-<nav class="navbar navbar-expand-ls navbar-dark bg-dark mb-3">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="<?= URLROOT;?>"><?= SITENAME;?></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                <li class="nav-item">
+<nav class="">
+    <div class="nav-container">
+        <a class="" href="<?= URLROOT;?>"><?= SITENAME;?></a>
+        <div class="" id="">
+            <ul class="">
+                <li class="">
                     <a class="nav-link" aria-current="page" href="<?= URLROOT;?>">Home</a>
                 </li>
-                <li class="nav-item">
+                <li class="">
                     <a class="nav-link" href="<?= URLROOT;?>/pages/about">About</a>
                 </li>
             </ul>
-            <ul class="navbar-nav ml-auto mb-2 mb-md-0">
+            <ul class="">
                 <?php if(isset($_SESSION['user_id'])): ?>
-                <li class="nav-item">
+                <li class="">
                     <a class="nav-link" aria-current="page" href="#">Welcome <?= $_SESSION['user_name'];?></a>
                 </li>
                 <!-- logout -->
-                <li class="nav-item">
+                <li class="">
                     <a class="nav-link" aria-current="page" href="<?= URLROOT;?>/users/logout">Logout</a>
                 </li>
 
                 <?php else : ?>
 
-                <li class="nav-item">
+                <li class="">
                     <a class="nav-link" aria-current="page" href="<?= URLROOT;?>/users/register">Register</a>
                 </li>
-                <li class="nav-item">
+                <li class="">
                     <a class="nav-link" href="<?= URLROOT;?>/users/login">Login</a>
                 </li>
                 <?php endif;?>
