90161f88795ce305100d1d9fc53e11f29de8a0c5
nathanjh-28 <nathan@nathanjharris.com>
2021-02-04 21:54:49 -0700

    test4

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -49,7 +49,6 @@ class Core
 
   public function getUrl()
   {
-    die(var_dump(isset($_GET['url'])));
     if (isset($_GET['url'])) {
       $url = rtrim($_GET['url'], '/');
       $url = filter_var($url, FILTER_SANITIZE_URL);
