7459af7b1357063b66f60e6e74ca51fa9e4b387e
nathanjh-28 <nathan@nathanjharris.com>
2021-02-04 21:46:56 -0700

    testing something

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -14,6 +14,7 @@ class Core
   {
     //print_r($this->getUrl());
     $url = $this->getUrl();
+    die(var_dump($url));
 
     // Look in controllers for first value
     if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
